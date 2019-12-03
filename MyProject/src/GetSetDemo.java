24c846a2dd3bdfddc8636092af8584261d6c1b03
Admin <Admin@DESKTOP-016R919>
2019-12-03 14:41:19 +0530

    this a task project

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,19 @@
+
+public class GetSetDemo {
+	string MyBankName;
+	int MyBankAcountNumber;
+	public string getMyBankName() {
+		return MyBankName;
+	}
+	public void setMyBankName(string myBankName) {
+		MyBankName = myBankName;
+	}
+	public int getMyBankAcountNumber() {
+		return MyBankAcountNumber;
+	}
+	public void setMyBankAcountNumber(int myBankAcountNumber) {
+		MyBankAcountNumber = myBankAcountNumber;
+	}
+	
+
+}
